e6e5f533a3c7fe5d7b7ee0e1f0849f55ec4a47f5
Dustin <heaps12345@gmail.com>
2018-02-09 13:55:48 -0500

    finishing touches

---
+++ comic-book-gallery-master/ComicBookGallery/Components/HtmlExtensionsImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ComicBookGallery
+{
+    public static class HtmlExtensionsImage
+    {
+        public static MvcHtmlString Image(this HtmlHelper htmlHelper,
+           string src, string altText, string cssClass)
+        {
+            TagBuilder tb = new TagBuilder("img");
+
+            tb.MergeAttribute("src", "/Images/" + src);
+            tb.MergeAttribute("alt", altText);
+
+            if (!string.IsNullOrWhiteSpace(cssClass))
+            {
+                tb.AddCssClass(cssClass);
+            }
+
+            return MvcHtmlString.Create(
+                tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
